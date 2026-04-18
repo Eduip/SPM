b8b37b4add548d5bcd5377befd505b183ad2e2ea
Eduardo Rubilar <e.rubilar03@gmail.com>
2026-04-17 22:48:07 -0400

    fix: restore postulation continue action

--- components/creacion-formulacion/postulacion/PostulacionFormContainer.tsx
+++ components/creacion-formulacion/postulacion/PostulacionFormContainer.tsx
@@ -3,6 +3,7 @@
 import { useState } from 'react'
 import PostulacionHeader from './PostulacionHeader'
 import PostulacionStepper from './PostulacionStepper'
+import PostulacionProgressPanel from './PostulacionProgressPanel'
 import { savePostulacionData } from '../../../app/creacion-formulacion/postulacion/actions'
 import FuenteFinanciamientoSelector from './FuenteFinanciamientoSelector'
 
@@ -134,6 +135,8 @@ export default function PostulacionFormContainer({
           campos={camposFuente}
           respuestasIniciales={respuestasIniciales ?? []}
         />
+
+        <PostulacionProgressPanel proyectoId={proyectoId} />
       </div>
     </>
   )
